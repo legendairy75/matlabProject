2cfadf1b002011dbdc6dddda6b6ad6b1be46c0c9
Cameron Gibson <36999898+legendairy75@users.noreply.github.com>
2022-11-09 13:22:23 -0500

    Add files via upload

--- outputDist.m
+++ outputDist.m
@@ -9,6 +9,10 @@ ultsonObj = ultrasonic(a,'D2','D4'); %assigns the ultrasonic sensor to ultsonObj
 %disp(distance)
 line = readVoltage(a,'A0'); % photo rsistor
 
+playTone(a,'D3',440,2);
+playTone(a,'D3',430,2);
+playTone(a,'D3',450,2);
+
 while 1
 
     distance = readDistance(ultsonObj); %reads the ultrasonic sensor (ultsonObj) as a distance and assignes the value to distance
@@ -17,21 +21,28 @@ while 1
 
     if distance < .2 % 5 is just a place holder, change before exicution
 
-    writeDigitalPin(a,'D6',1); % LED for photoresister on
+        %load("tripWire.m")
+
+   % writeDigitalPin(a,'D6',1); % LED for photoresister on
+
+    
 
 % photo resistor start
-        while 1
+        %while 1%distance < .2
     
-            line = readVoltage(a,'A0'); % photo rsistor
+            %line = readVoltage(a,'A0'); % photo rsistor
     
-            if line<1
+          %  if line<1
     
-                writeDigitalPin(a,'D13',1);
-                playTone(a,'D3',440,2);
+          %      writeDigitalPin(a,'D13',1);
+         %       playTone(a,'D3',440,2);
+%
+         %   elseif distance > .2
+        %        break
             
-            end
-        
-        end
+       %     end
+
+      %  end
 %Photoresister stop
 
     else
